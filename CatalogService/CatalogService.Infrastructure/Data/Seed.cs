5646a04272dfa42d4bddc7c4f37df7405ce3c2ff
ALTa <andrik20144102@gmail.com>
2023-10-16 13:45:55 +0300

    A seeding improved

--- CatalogService/CatalogService.Infrastructure/Data/Seed.cs
+++ CatalogService/CatalogService.Infrastructure/Data/Seed.cs
@@ -1,4 +1,5 @@
-﻿using CatalogService.Infrastructure.Data.ApplicationDbContext;
+﻿using CatalogService.Domain.Entities;
+using CatalogService.Infrastructure.Data.ApplicationDbContext;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.DependencyInjection;
 
@@ -17,9 +18,135 @@ namespace CatalogService.Infrastructure.Data
         {
             using (var scope = _serviceScopeFactory.CreateScope())
             {
-                var _dbContext = scope.ServiceProvider.GetRequiredService<CatalogServiceDbContext>();
+                var dbContext = scope.ServiceProvider.GetRequiredService<CatalogServiceDbContext>();
 
-                _dbContext.Database.Migrate();
+                dbContext.Database.Migrate();
+
+                if (!dbContext.Restaurants.Any())
+                {
+                    var milkFoodType = new FoodType()
+                    {
+                        Name = "Milk product",
+                    };
+
+                    var chocolateFoodType = new FoodType()
+                    {
+                        Name = "Chocolate product",
+                    };
+
+                    var bakeFoodType = new FoodType()
+                    {
+                        Name = "Bake product",
+                    };
+
+                    var menu = new List<Menu>
+                    {
+                        new Menu
+                        {
+                            FoodName = "Milk",
+                            Cost = 1,
+                            FoodType = milkFoodType,
+                        },
+                        new Menu
+                        {
+                            FoodName = "Cookies",
+                            Cost = 5,
+                            FoodType = bakeFoodType,
+                        },
+                        new Menu
+                        {
+                            FoodName = "Chocolate",
+                            Cost = 3,
+                            FoodType = chocolateFoodType,
+                        },
+                    };
+
+                    var restaurants = new List<Restaurant>
+                    {
+                        new Restaurant()
+                        {
+                            Name = "DeliciousFood",
+                            City = "London",
+                            Street = "Oxford",
+                            House = "25",
+                            Employees = new List<Employee>
+                            {
+                                new Employee
+                                {
+                                    Id = 1,
+                                    Name = "andrei",
+                                },
+                                new Employee
+                                {
+                                    Id = 2,
+                                    Name = "arthur",
+                                },
+                                new Employee
+                                {
+                                    Id = 3,
+                                    Name = "maksim",
+                                }
+                            },
+                            Menu = menu,
+                        },
+                        new Restaurant()
+                        {
+                            Name = "SpicyFood",
+                            City = "London",
+                            Street = "Oxford",
+                            House = "26",
+                            Employees = new List<Employee>
+                            {
+                                new Employee
+                                {
+                                    Id = 4,
+                                    Name = "dmitriy",
+                                },
+                                new Employee
+                                {
+                                    Id = 5,
+                                    Name = "nikolai",
+                                },
+                                new Employee
+                                {
+                                    Id = 6,
+                                    Name = "yaroslav",
+                                }
+                            },
+                            Menu = menu,
+                        },
+                        new Restaurant()
+                        {
+                            Name = "Menu for you",
+                            City = "London",
+                            Street = "Oxford",
+                            House = "27",
+                            Employees = new List<Employee>
+                            {
+                                new Employee
+                                {
+                                    Id = 7,
+                                    Name = "ilya",
+                                },
+                                new Employee
+                                {
+                                    Id = 8,
+                                    Name = "anya",
+                                },
+                                new Employee
+                                {
+                                    Id = 9,
+                                    Name = "gleb",
+                                }
+                            },
+                            Menu = menu,
+                        },
+                    };
+
+                    dbContext.Restaurants.AddRange(restaurants);
+                }
+
+                dbContext.SaveChanges();
             }
         }
     }
