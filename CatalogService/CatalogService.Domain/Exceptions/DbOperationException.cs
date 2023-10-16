ad5604784da529e870dd9b5c4a1d687ac3517d1c
ALTa <andrik20144102@gmail.com>
2023-10-16 09:41:59 +0300

    A string formatting added

--- CatalogService/CatalogService.Domain/Exceptions/DbOperationException.cs
+++ CatalogService/CatalogService.Domain/Exceptions/DbOperationException.cs
@@ -3,7 +3,7 @@
     public class DbOperationException : Exception
     {
         public DbOperationException(string methodName, string key, Type type)
-            : base((ExceptionMessages.DbOperationExceptionMessage, methodName, type.Name, key).ToString())
+            : base(string.Format(ExceptionMessages.DbOperationExceptionMessage, methodName, type.Name, key))
         {
         }
     }
--- CatalogService/CatalogService.Domain/Exceptions/NotFoundException.cs
+++ CatalogService/CatalogService.Domain/Exceptions/NotFoundException.cs
@@ -3,7 +3,7 @@
     public class NotFoundException : Exception
     {
         public NotFoundException(string keyOwner, string key, Type type)
-            : base((ExceptionMessages.NotFoundExceptionMessage, type.Name, key, keyOwner).ToString())
+            : base(string.Format(ExceptionMessages.NotFoundExceptionMessage, type.Name, key, keyOwner))
         {
         }
     }
