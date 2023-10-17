ffb46a96ff563985653f28ceaf43afa6b62355b1
ALTa <andrik20144102@gmail.com>
2023-10-17 17:35:15 +0300

    Clean architecture has been implemented

---
+++ BookingService/BookingService.Presentation/Configurations/DbConfiguration.cs
@@ -0,0 +1,21 @@
+﻿namespace BookingService.Presentation.Configurations
+{
+    public static class DbConfiguration
+    {
+        public static IServiceCollection AddDatabaseContext(this IServiceCollection services,
+            WebApplicationBuilder builder)
+        {
+            services.AddDbContext<>(options =>
+            {
+                if (builder.Environment.IsDevelopment())
+                {
+                }
+                else
+                {
+                }
+            });
+
+            return services;
+        }
+    }
+}
---
+++ BookingService/BookingService.Presentation/Configurations/ServicesConfiguration.cs
@@ -0,0 +1,31 @@
+﻿using BookingService.Application.ServicesConfigurations;
+
+namespace BookingService.Presentation.Configurations
+{
+    public static class ServicesConfiguration
+    {
+        public static IServiceCollection ConfigureServices(this IServiceCollection services,
+            WebApplicationBuilder builder)
+        {
+            services.AddControllers();
+
+            services.AddDatabaseContext(builder);
+
+            services.AddEndpointsApiExplorer();
+
+            services.AddSwagger();
+
+            services.AddSingleton<Seed>();
+
+            services.AddMapper();
+
+            services.AddFluentValidation();
+
+            services.AddJwtTokenAuthConfiguration(builder);
+
+            services.AddAuthorization();
+
+            return services;
+        }
+    }
+}
