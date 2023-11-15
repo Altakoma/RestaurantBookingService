03ec6edecc10e5adfceea9d536497af8ae23f819
Altakoma <andrik20144102@gmail.com>
2023-11-15 16:21:41 +0300

    Bug fixed

--- OrderService/OrderService.Application/MediatR/Order/Commands/UpdateOrderBySynchronizationCommand.cs
+++
@@ -1,9 +0,0 @@
-﻿using MediatR;
-
-namespace OrderService.Application.MediatR.Order.Commands
-{
-    public class UpdateOrderBySynchronizationCommand : IRequest
-    {
-        public int Id { get; set; }
-    }
-}
